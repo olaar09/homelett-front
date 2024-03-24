e05e6973cc75604c1a41e5500507863ad87b8d6c
olaar09 <agboolar09@gmail.com>
2024-03-24 10:58:44 +0100

    Versioning untracked files

--- src/app/components/Auth/GoogleSignin.tsx
+++ src/app/components/Auth/GoogleSignin.tsx
@@ -1,21 +1,26 @@
 "use client";
 import React from "react";
 import { auth } from "../../../utils/firebaseConfig"; // Adjust the path as necessary
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const GoogleSignIn = () => {
   const signInWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
-      const result = await signInWithPopup(auth, provider);
+      const result = await signInWithRedirect(auth, provider);
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential!.accessToken;
+      console.log("Google signin successful", token);
       // The signed-in user info.
-      const user = result.user;
+      //  const user = result.user;
       // You can now use this to authenticate with your backend or just use it client-side
-      console.log(user);
+      // console.log(user);
     } catch (error) {
       console.error(error);
     }
