6f9087d7d1a7388d0817aa76fdc33de29ad20538
olaar09 <agboolar09@gmail.com>
2024-04-08 15:27:55 +0100

    ..

--- src/app/home/_components/SelectChatDrawer.tsx
+++ src/app/home/_components/SelectChatDrawer.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Drawer, List, Button } from "antd";
 import { IChat } from "@/app/interfaces/IChatItem";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 // Define types for the component props
 interface ChatListDrawerProps {
@@ -32,8 +33,30 @@ const ChatListDrawer: React.FC<ChatListDrawerProps> = ({
         <List
           dataSource={items}
           renderItem={(item) => (
-            <List.Item onClick={() => handleItemClick(item)}>
-              {item.slug} {/* Assuming each item has a 'name' property */}
+            <List.Item
+              className="hover:opacity-75 cursor-pointer"
+              onClick={() => handleItemClick(item)}
+            >
+              <List.Item.Meta
+                avatar={
+                  <Icon
+                    icon={item.datasource.source_type.icon}
+                    className="text-2xl w-10"
+                  />
+                }
+                title={
+                  <div className=" items-center w-full flex justify-between">
+                    <span className={"text-foreground"}>
+                      {item.datasource.name}
+                    </span>
+
+                    <span className="text-foreground text-xs py-1 px-2 rounded-xl">
+                      <Icon icon={"ep:arrow-right"} className="text-xl w-10" />
+                    </span>
+                  </div>
+                }
+                description={<span>{item.slug ?? "No title yet"}</span>}
+              />
             </List.Item>
           )}
         />
