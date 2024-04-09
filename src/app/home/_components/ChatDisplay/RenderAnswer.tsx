c292ff0191a3d289e11ed79b2a16c8c276cd991f
olaar09 <agboolar09@gmail.com>
2024-04-09 10:07:02 +0100

    text to whit

--- src/app/home/_components/ChatDisplay/RenderAnswer.tsx
+++ src/app/home/_components/ChatDisplay/RenderAnswer.tsx
@@ -4,6 +4,27 @@ import { Icon } from "@iconify/react";
 
 const { Meta } = Card;
 
+const TextContentDisplay = ({
+  avatar,
+  senderName,
+  content,
+}: {
+  avatar: string;
+  senderName: string;
+  content: string;
+}) => {
+  return (
+    <div className="flex flex-col gap-y-3 my-4 hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2 w-full">
+      <div className="flex items-center gap-x-2">
+        <Icon icon={avatar} className="bg-transparent text-3xl text-gray-600" />
+        <span className="text-foreground font-bold">{senderName}</span>
+      </div>
+
+      <span className=" text-[0.85rem]">{content}</span>
+    </div>
+  );
+};
+
 const TableContentDisplay = ({
   data,
   columns,
@@ -58,7 +79,7 @@ const RenderAnswer: React.FC<RenderAnswerProps> = ({
   try {
     const parse = JSON.parse(content);
     const headers = Object.keys(parse);
-
+    // single table
     if (Array.isArray(parse) && parse.length > 0) {
       console.log(Object.keys(parse[0]), parse);
       const columns = Object.keys(parse[0]).map((x) => {
@@ -69,22 +90,24 @@ const RenderAnswer: React.FC<RenderAnswerProps> = ({
         };
       });
       return <TableContentDisplay data={parse} columns={columns} />;
-    } else if (true) {
-      return <div>come back to this</div>;
+    } else if (headers.length > 0 && Array.isArray(parse[headers[0]])) {
+      return <div>Multiple tables</div>;
+    } else {
+      return (
+        <TextContentDisplay
+          avatar={avatar}
+          senderName={senderName}
+          content={content}
+        />
+      );
     }
   } catch (error) {
     return (
-      <div className="flex flex-col gap-y-3 my-4 hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2">
-        <div className="flex items-center gap-x-2">
-          <Icon
-            icon={avatar}
-            className="bg-transparent text-3xl text-gray-600"
-          />
-          <span className="text-foreground font-bold">{senderName}</span>
-        </div>
-
-        <span className=" text-[0.85rem]">{content}</span>
-      </div>
+      <TextContentDisplay
+        avatar={avatar}
+        senderName={senderName}
+        content={content}
+      />
     );
   }
 };
