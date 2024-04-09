5a7ca0d8ddcec78071f29206cf423b433135eb94
olaar09 <agboolar09@gmail.com>
2024-04-09 09:55:23 +0100

    text to whit

--- src/app/home/_components/ChatDisplay/RenderAnswer.tsx
+++ src/app/home/_components/ChatDisplay/RenderAnswer.tsx
@@ -1,8 +1,46 @@
 import React from "react";
-import { Avatar, Card } from "antd";
+import { Avatar, Card, Table } from "antd";
 import { Icon } from "@iconify/react";
 
 const { Meta } = Card;
+
+const TableContentDisplay = ({
+  data,
+  columns,
+}: {
+  data: any;
+  columns: any;
+}) => {
+  return (
+    <div className=" w-full">
+      <Table
+        dataSource={data}
+        columns={columns}
+        rowKey="id"
+        pagination={false}
+        bordered
+        size="small"
+        // scroll={{ x: columns.length > 5 ? 3300 : undefined }}
+        rowClassName={() => "text-xs p-0 m-0 "}
+      />
+    </div>
+  );
+};
+
+const Header = ({
+  avatar,
+  senderName,
+}: {
+  avatar: string;
+  senderName: string;
+}) => {
+  return (
+    <div className="flex items-center gap-x-2">
+      <Icon icon={avatar} className="bg-transparent text-3xl text-gray-600" />
+      <span className="text-foreground font-bold">{senderName}</span>
+    </div>
+  );
+};
 
 interface RenderAnswerProps {
   avatar: string;
@@ -15,16 +53,40 @@ const RenderAnswer: React.FC<RenderAnswerProps> = ({
   content,
   senderName,
 }) => {
-  return (
-    <div className="flex flex-col gap-y-3 my-4 hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2">
-      <div className="flex items-center gap-x-2">
-        <Icon icon={avatar} className="bg-transparent text-3xl text-gray-600" />
-        <span className="text-foreground font-bold">{senderName}</span>
-      </div>
+  let parsable;
 
-      <span className=" text-[0.85rem]">{content}</span>
-    </div>
-  );
+  try {
+    const parse = JSON.parse(content);
+    const headers = Object.keys(parse);
+
+    if (Array.isArray(parse) && parse.length > 0) {
+      console.log(Object.keys(parse[0]), parse);
+      const columns = Object.keys(parse[0]).map((x) => {
+        return {
+          title: x,
+          dataIndex: x,
+          key: x,
+        };
+      });
+      return <TableContentDisplay data={parse} columns={columns} />;
+    } else if (true) {
+      return <div>come back to this</div>;
+    }
+  } catch (error) {
+    return (
+      <div className="flex flex-col gap-y-3 my-4 hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2">
+        <div className="flex items-center gap-x-2">
+          <Icon
+            icon={avatar}
+            className="bg-transparent text-3xl text-gray-600"
+          />
+          <span className="text-foreground font-bold">{senderName}</span>
+        </div>
+
+        <span className=" text-[0.85rem]">{content}</span>
+      </div>
+    );
+  }
 };
 
 export default RenderAnswer;
