d72e2463d84a58924d8b5080faff334983d4f73b
olaar09 <agboolar09@gmail.com>
2024-04-09 10:20:17 +0100

    text to whit

--- src/app/home/_components/ChatDisplay/RenderAnswer.tsx
+++ src/app/home/_components/ChatDisplay/RenderAnswer.tsx
@@ -4,50 +4,6 @@ import { Icon } from "@iconify/react";
 
 const { Meta } = Card;
 
-const TextContentDisplay = ({
-  avatar,
-  senderName,
-  content,
-}: {
-  avatar: string;
-  senderName: string;
-  content: string;
-}) => {
-  return (
-    <div className="flex flex-col gap-y-3 my-4 hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2 w-full">
-      <div className="flex items-center gap-x-2">
-        <Icon icon={avatar} className="bg-transparent text-3xl text-gray-600" />
-        <span className="text-foreground font-bold">{senderName}</span>
-      </div>
-
-      <span className=" text-[0.85rem]">{content}</span>
-    </div>
-  );
-};
-
-const TableContentDisplay = ({
-  data,
-  columns,
-}: {
-  data: any;
-  columns: any;
-}) => {
-  return (
-    <div className=" w-full">
-      <Table
-        dataSource={data}
-        columns={columns}
-        rowKey="id"
-        pagination={false}
-        bordered
-        size="small"
-        // scroll={{ x: columns.length > 5 ? 3300 : undefined }}
-        rowClassName={() => "text-xs p-0 m-0 "}
-      />
-    </div>
-  );
-};
-
 const Header = ({
   avatar,
   senderName,
@@ -60,6 +16,48 @@ const Header = ({
       <Icon icon={avatar} className="bg-transparent text-3xl text-gray-600" />
       <span className="text-foreground font-bold">{senderName}</span>
     </div>
+  );
+};
+
+const TextContentDisplay = ({
+  avatar,
+  senderName,
+  content,
+}: {
+  avatar: string;
+  senderName: string;
+  content: string;
+}) => {
+  return (
+    <div className="flex flex-col gap-y-3 my-4 hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2 w-full">
+      <Header avatar={avatar} senderName={senderName} />
+      <span className=" text-[0.85rem]">{content}</span>
+    </div>
+  );
+};
+
+const TableContentDisplay = ({
+  data,
+  columns,
+  avatar,
+  senderName,
+}: {
+  data: any;
+  columns: any;
+  senderName: string;
+  avatar: string;
+}) => {
+  return (
+    <Table
+      dataSource={data}
+      columns={columns}
+      rowKey="id"
+      pagination={false}
+      bordered
+      size="small"
+      // scroll={{ x: columns.length > 5 ? 3300 : undefined }}
+      rowClassName={() => "text-xs p-0 m-0 "}
+    />
   );
 };
 
@@ -89,9 +87,44 @@ const RenderAnswer: React.FC<RenderAnswerProps> = ({
           key: x,
         };
       });
-      return <TableContentDisplay data={parse} columns={columns} />;
+      return (
+        <div className=" flex flex-col gap-y-3  hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2 w-full">
+          <Header avatar={avatar} senderName={senderName} />
+          <TableContentDisplay
+            data={parse}
+            columns={columns}
+            senderName={senderName}
+            avatar={avatar}
+          />
+        </div>
+      );
     } else if (headers.length > 0 && Array.isArray(parse[headers[0]])) {
-      return <div>Multiple tables</div>;
+      const ui = [];
+      for (const index in headers) {
+        if (Array.isArray(parse[headers[index]])) {
+          const columns = Object.keys(parse[headers[index]]).map((x) => {
+            return {
+              title: x,
+              dataIndex: x,
+              key: x,
+            };
+          });
+          ui.push(
+            <TableContentDisplay
+              data={parse[headers[index]]}
+              columns={columns}
+              senderName={senderName}
+              avatar={avatar}
+            />
+          );
+        }
+      }
+      return (
+        <div className=" flex flex-col gap-y-3  hover:bg-gray-100 cursor-pointer px-2 rounded-lg py-2 w-full">
+          <Header avatar={avatar} senderName={senderName} />
+          {ui}
+        </div>
+      );
     } else {
       return (
         <TextContentDisplay
