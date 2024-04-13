2e1607e7b4c9a2a8a7ba1bbe007c62071745e60b
olaar09 <agboolar09@gmail.com>
2024-04-13 22:16:08 +0100

    added randomness

--- src/app/home/_components/DBChat/QueryDisplay/index.tsx
+++ src/app/home/_components/DBChat/QueryDisplay/index.tsx
@@ -16,7 +16,7 @@ const QueryDisplay = ({
       footer={null}
       title={"Generated query"}
     >
-      <div className="p-3 flex flex-col mx-auto gap-y-2">
+      <div className="py-3 flex flex-col mx-auto gap-y-2">
         <div>
           <div className="bg-gray-200 rounded-md my-4 p-2">
             <span className="text-gray-700 text-xs"> {query}</span>
