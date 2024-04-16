e9466d9a17d0bb7100132bc42161432c6b59c9d2
olaar09 <agboolar09@gmail.com>
2024-04-16 17:50:46 +0100

    add output

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   manifest: "/manifest.json",
-  title: "PostBird",
+  title: "PostBird | AI Co-Pilot for job search",
   description: "AI Co-Pilot for your job search",
   applicationName: "PostBird",
   appleWebApp: true,
