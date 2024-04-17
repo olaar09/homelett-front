41861440657e1abf3762a8aea17634ae39885a7c
olaar09 <agboolar09@gmail.com>
2024-04-17 09:28:02 +0100

    ..

--- src/app/components/Landing/Tilt.tsx
+++ src/app/components/Landing/Tilt.tsx
@@ -4,7 +4,7 @@ const FlipOnScroll = ({ rotationDegrees }: any) => {
   return (
     <div className="perspective-800 h-52 lg:h-[500px] w-11/12 mx-auto">
       <div
-        className={`transform ${rotationDegrees} h-52 lg:h-[500px]  rounded-lg  transition-all duration-300 w-full bg-no-repeat bg-center bg-contain bg-[url('/home_her.png')]`}
+        className={`transform ${rotationDegrees} h-52 lg:h-[500px]  rounded-lg  transition-all duration-300 w-full bg-no-repeat bg-center bg-contain bg-[url('/hom_rec.png')]`}
       ></div>
     </div>
   );
