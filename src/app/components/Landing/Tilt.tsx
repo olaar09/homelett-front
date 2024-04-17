22519a532dd6c66db7180485dea672c3093af85a
olaar09 <agboolar09@gmail.com>
2024-04-17 08:23:59 +0100

    ..

--- src/app/components/Landing/Tilt.tsx
+++ src/app/components/Landing/Tilt.tsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 
 const FlipOnScroll = ({ rotationDegrees }: any) => {
   return (
-    <div className="perspective-800 h-52 lg:h-[500px] w-10/12 mx-auto">
+    <div className="perspective-800 h-52 lg:h-[500px] w-11/12 mx-auto">
       <div
-        className={`transform ${rotationDegrees} h-52 lg:h-[500px]  rounded-lg  transition-all duration-300 w-full bg-no-repeat bg-center bg-contain bg-[url('/home_hero.png')]`}
+        className={`transform ${rotationDegrees} h-52 lg:h-[500px]  rounded-lg  transition-all duration-300 w-full bg-no-repeat bg-center bg-contain bg-[url('/home_her.png')]`}
       ></div>
     </div>
   );
