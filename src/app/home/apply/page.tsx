5911569aa477a22c3bff32c5a86b49bbd86877fa
olaar09 <agboolar09@gmail.com>
2024-04-18 01:16:34 +0100

    inm

--- src/app/home/apply/page.tsx
+++ src/app/home/apply/page.tsx
@@ -234,10 +234,34 @@ const Chat = () => {
                     <span className="font-black text-xl">Experiences</span>
                   </div>
 
-                  <ExperienceItem title={""} duration={""} content={""} />
-                  <ExperienceItem title={""} duration={""} content={""} />
-                  <ExperienceItem title={""} duration={""} content={""} />
-                  <ExperienceItem title={""} duration={""} content={""} />
+                  <ExperienceItem
+                    title={"Software developer/DevOps Consultant (Contract)"}
+                    duration={"Open AI (January 2023 - Present)"}
+                    content={
+                      "Open AI provides a smooth experience to Africans applying to colleges abroad through thier online platform. Helping students from 70 plus high schools apply/montitor college applications directly to 900 + universities around Europe, Aisa and North America. I joined a team of 4 developers to help oversee the platfrom move from current monolith application to an API based architecture running on modern cloud infrastructures. The stack i use day to day include PHP 7/Laravel, Javascript / React JS, Nginx, Ubuntu 18 (Running on Digital ocean & AWS platforms), Ansible and Docker"
+                    }
+                  />
+                  <ExperienceItem
+                    title={"Fullstack developer"}
+                    duration={"Paystack Africa (Feb 2019 - August 2019)"}
+                    content={
+                      " Paystack Africa is a talent accelerator organization offering tools and products to help graduates, professionals and entrepreneurs become better in their career. I work with a team of four to develop internal tools to help scan through proles of graduates and entrepreneurs and match them to experienced professionals for mentoring. My day to day tech includes, Typescript/NodeJS (NestJs), Typescript/ReactJs, React native, Sass, Redux, Redux saga, Mocha, Enzymes, NodeJs, Firebase, Travis CI, Coveralls, Ubuntu, Laravel"
+                    }
+                  />
+                  <ExperienceItem
+                    title={"Android fullstack engineer"}
+                    duration={"GidiBooks.com (Nov 2017 - Feb 2019)"}
+                    content={
+                      "GidiBooks  Is an ebook publishing startup with over 500,000 user base and over 30,000 monthly users. I work as part of a mid sized team of frontend and backend engineers to improve usablility of the web and android platforms as well as build tools to improve publishing and reading experience of our users. My day to day stack includes ReactJS, Redux, Redux Saga, Mocha, Enzymes, Travic CI, Coveralls Mobx, NextJs, CSS3, Sass, NodeJs, Express, Loopback, PHP, PhpUnit, Laravel, AWS services, Microsoft Azure, Ubuntu"
+                    }
+                  />
+                  <ExperienceItem
+                    title={"Software engineer (Remote)"}
+                    duration={"Codaye technologies (2015) 6 months"}
+                    content={
+                      "codaye.com Is a software firm that specializes in developing tech solutions such as website, web applications, mobile applications. At codaye, i worked remotely with a team of mobile and web engineers to produce various solutions."
+                    }
+                  />
                 </section>
               </div>
 
