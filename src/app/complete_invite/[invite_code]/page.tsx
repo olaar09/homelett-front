bc4ac38337c3f1b1ee1326a5aa9aa07469482cc5
olaar09 <agboolar09@gmail.com>
2024-04-21 09:55:04 +0100

    ...

--- src/app/complete_invite/[invite_code]/page.tsx
+++ src/app/complete_invite/[invite_code]/page.tsx
@@ -122,15 +122,15 @@ export default function CompleteInvite() {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <title>SequelBase | Complete invite</title>
-        <meta name="description" content="Sequelbase" />
+        <title>ApplyGenius | Complete invite</title>
+        <meta name="description" content="ApplyGenius" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
 
         <link rel="icon" href="/database_logo_smaller.png" />
-        <link rel="canonical" href="https://app.sequelbase.com/connect" />
+        <link rel="canonical" href="https://app.ApplyGeni.us/connect" />
       </Head>
       <div className="bg-white">
         <main className="h-screen bg-white  mx-auto  w-full flex ">
@@ -174,7 +174,7 @@ export default function CompleteInvite() {
                       className="text-gray-800 text-lg ml-1 font-black"
                     >
                       {" "}
-                      SequelBase
+                      ApplyGenius
                     </h1>
                   </div>
                   <span className="text-gray-600 text-sm">
@@ -251,7 +251,7 @@ export default function CompleteInvite() {
                     <ACButton
                       onClick={onSubmitLogin}
                       loading={submittingInvite}
-                      text={`Join ${inviteData?.company_name} on Sequelbase`}
+                      text={`Join ${inviteData?.company_name} on ApplyGenius`}
                       type={"button"}
                       children={undefined}
                     />
