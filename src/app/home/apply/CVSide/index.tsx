1e648f020013a2c620e0a24107667728c89ccbe5
olaar09 <agboolar09@gmail.com>
2024-04-28 10:22:31 +0100

    rearranged text

--- src/app/home/apply/CVSide/index.tsx
+++ src/app/home/apply/CVSide/index.tsx
@@ -22,44 +22,58 @@ const AsideItem2 = ({ title, desc }: any) => {
 //"bi:github"
 
 const ASide = ({ profile }: any) => {
+  const skills =
+    profile?.attributes?.filter((attr: any) => attr.attribute === "skills") ??
+    [];
+
+  const languages =
+    profile?.attributes?.filter((attr: any) => attr.attribute === "language") ??
+    [];
+
+  const links =
+    profile?.attributes?.filter((attr: any) => attr.attribute === "links") ??
+    [];
+
+  const academics =
+    profile?.attributes?.filter(
+      (attr: any) => attr.attribute === "academics"
+    ) ?? [];
+
+  console.log(skills, " joeldkms;");
+
   return (
     <div className="bg-gray-200 h-full w-4/12  ">
       <HeaderSide />
 
       <div className="flex flex-col gap-y-6 mt-10  px-10 mx-auto w-11/12">
-        <AsideItem title={profile?.niche_email} icon={"mage:email-fill"} />
+        {(links ?? []).map((link: any, key: any) => (
+          <AsideItem2 key={key} desc={link.value} title={link.icon} />
+        ))}
+
+        {/*         <AsideItem title={profile?.niche_email} icon={"mage:email-fill"} />
         <AsideItem title={profile?.niche_phone} icon={"mdi:telephone"} />
         <AsideItem
           title={"linkedin.com/in/isaac09/"}
           icon={"akar-icons:linkedin-v1-fill"}
         />
-        <AsideItem title={profile?.link_1} icon={profile?.link_1_icon} />
+        <AsideItem title={profile?.link_1} icon={profile?.link_1_icon} /> */}
       </div>
 
       <div className="mt-10  mx-auto w-11/12 px-10 ">
         <span className="font-black">Education</span>
         <div className="flex flex-col gap-y-4 mt-4 ">
-          <AsideItem2
-            desc={profile?.link_1_date}
-            title={profile?.link_1_title}
-          />
-          <AsideItem2
-            desc={profile?.link_2_date}
-            title={profile?.link_2_title}
-          />
+          {academics.map((academic: any, key: any) => (
+            <AsideItem2 key={key} desc={academic.value} title={academic.icon} />
+          ))}
         </div>
       </div>
 
       <div className="mt-10  mx-auto w-11/12 px-10 ">
         <span className="font-black">Skills</span>
         <div className="flex flex-col gap-y-4 mt-4 ">
-          {profile?.skills ??
-            [].map((sk) => (
-              <AsideItem2
-                desc={"React, Nuxt, Next, Vue"}
-                title={"Javascript"}
-              />
-            ))}
+          {skills.map((sk: any, key: any) => (
+            <AsideItem2 key={key} desc={sk.value} title={sk.icon} />
+          ))}
           {/*          <AsideItem2 desc={"Flask, Django"} title={"Python"} />
           <AsideItem2 desc={"Laravel, YII"} title={"PHP"} /> */}
         </div>
@@ -68,8 +82,9 @@ const ASide = ({ profile }: any) => {
       <div className="mt-10  mx-auto w-11/12 px-10 ">
         <span className="font-black">Languages</span>
         <div className="flex flex-col gap-y-4 mt-4 ">
-          {profile?.languages ??
-            [].map((sk) => <AsideItem2 desc={"Native"} title={"Yoruba"} />)}
+          {languages.map((lang: any, key: any) => (
+            <AsideItem2 key={key} desc={lang.value} title={lang.icon} />
+          ))}
         </div>
       </div>
     </div>
