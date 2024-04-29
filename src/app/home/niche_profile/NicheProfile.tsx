69a0e9b343eb7690860ffaed0d7f74571d24fbcf
olaar09 <agboolar09@gmail.com>
2024-04-29 23:20:15 +0100

    ..

--- src/app/home/niche_profile/NicheProfile.tsx
+++ src/app/home/niche_profile/NicheProfile.tsx
@@ -1,7 +1,59 @@
 "use client";
 
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { Drawer } from "antd";
+import { useState } from "react";
 
+const requiredDetails = [
+  {
+    isCompleted: false,
+    key: "basic",
+    title: "Bio information",
+    description:
+      "Basic information about you, including first name, last name and phone number",
+  },
+  {
+    isCompleted: false,
+    key: "academics",
+    title: "Academic information",
+    description:
+      "Information about your history. Also include any certificate or diploma from a traditional institution",
+  },
+  {
+    isCompleted: false,
+    key: "skills",
+    title: "Primary skills",
+    description:
+      "List all your primary skills that makes you valuable in the roles your looking for",
+  },
+  {
+    isCompleted: false,
+    key: "work",
+    title: "Work experience",
+    description:
+      "Information about your work history. Also include any internship, freelancing or contract roles",
+  },
+  {
+    isCompleted: false,
+    key: "socials",
+    title: "Social links",
+    description:
+      "Information about your social media presence such as linkedin, Github, etc",
+  },
+  {
+    isCompleted: false,
+    key: "awards",
+    title: "Accomplishments",
+    description:
+      "Such as awards, ground breaking accomplishments and recognitions",
+  },
+  {
+    isCompleted: false,
+    key: "settings",
+    title: "Settings",
+    description: "Add settings such as agent mode",
+  },
+];
 const NicheProfileDrawer = ({
   open,
   onClose,
@@ -9,6 +61,12 @@ const NicheProfileDrawer = ({
   onClose: any;
   open: boolean;
 }) => {
+  const [selected, setSelected] = useState<any>(null);
+
+  const onSelect = (info: any) => {
+    setSelected(info);
+  };
+
   return (
     <Drawer
       width={
@@ -18,11 +76,34 @@ const NicheProfileDrawer = ({
       }
       title="Manage profile"
       onClose={onClose}
+      className="px-0 py-0"
+      style={{ padding: 0 }}
       open={open}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <div className="w-full flex items-center h-screen px-0">
+        <div className="lg:w-[400px] w-full border-r h-full border-gray-200   ">
+          {requiredDetails.map((info) => (
+            <div
+              onClick={() => onSelect(info)}
+              className={`flex flex-col gap-y-2 h-32 hover:bg-gray-50 ${
+                selected?.key === info.key ? "bg-gray-100" : ""
+              } cursor-pointer px-4 py-4`}
+            >
+              <div className="flex justify-between w-full items-center pr-4">
+                <span className="text-md font-black">{info.title}</span>
+                {/*   <Icon
+                  icon={"lets-icons:check-fill"}
+                  className="text-xl text-green-700"
+                /> */}
+              </div>
+
+              <span className="text-sm text-gray-500">{info.description}</span>
+            </div>
+          ))}
+        </div>
+
+        <div className="lg:w-[400px] lg:flex-grow lg:flex hidden"></div>
+      </div>
     </Drawer>
   );
 };
