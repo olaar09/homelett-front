fa683f8c843a1f59f203bac309fcd8cfa6b2d492
olaar09 <agboolar09@gmail.com>
2024-05-10 09:07:41 +0100

    ..

--- src/app/components/Chip.tsx
+++ src/app/components/Chip.tsx
@@ -14,19 +14,17 @@ interface ChipProps {
 
 const Chip: React.FC<ChipProps> = ({ action, title, isSelected, icon }) => {
   return (
-    return (
-      <div
-        onClick={() => {
-          if (action) {
-            action(title);
-          }
-        }}
-        className="rounded-2xl bg-gray-200 px-2 flex items-center gap-x-2 hover:opacity-60 transition-all duration-100 cursor-pointer"
-      >
-        <span>{title}</span>
-        {action && <Icon icon={"lucide:plus"} />}
-      </div>
-    );>
+    <div
+      onClick={() => {
+        if (action) {
+          action(title);
+        }
+      }}
+      className="rounded-2xl bg-gray-200 px-2 flex items-center gap-x-2 hover:opacity-60 transition-all duration-100 cursor-pointer"
+    >
+      <span>{title}</span>
+      {action && <Icon icon={"lucide:plus"} />}
+    </div>
   );
 };
 
