70748c5ff01072f79230c396f7718e316b277cce
olaar09 <agboolar09@gmail.com>
2024-05-12 22:36:03 +0100

    ..

--- src/app/home/apply/MobileInsightDrawer/MobileInsightDrawer.tsx
+++ src/app/home/apply/MobileInsightDrawer/MobileInsightDrawer.tsx
@@ -35,7 +35,7 @@ const MobileInsightDrawer = ({
       closable={true}
       onClose={onClose}
       open={open}
-      height={screen.height - (screen.availHeight / 100) * 5}
+      height={screen.height - (screen.availHeight / 100) * 15}
       key={"bottom"}
     >
       <InsightSide
