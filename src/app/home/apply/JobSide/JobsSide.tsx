3adfcd24b0df37666fa153e98b7aa7d4d45a9b93
olaar09 <agboolar09@gmail.com>
2024-05-12 22:25:05 +0100

    ..

--- src/app/home/apply/JobSide/JobsSide.tsx
+++ src/app/home/apply/JobSide/JobsSide.tsx
@@ -36,8 +36,8 @@ export const JobsSide = ({
   const authContext = useContext(AuthContext);
 
   return (
-    <div className="lg:w-[500px] w-full h-full flex flex-col relative  border-l-0   pb-12 ">
-      <div className=" overflow-y-scroll w-full h-full  mb-2">
+    <div className="lg:w-[500px] w-full h-full flex flex-col relative  border-l-0   pb-0 ">
+      <div className=" overflow-y-scroll w-full h-full  mb-0">
         {(jobs ?? []).map((job: { id: any; name: any }) => (
           <JobItem
             job={job}
