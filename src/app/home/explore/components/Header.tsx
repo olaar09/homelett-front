fe6f85823b8c974f3b6167ab0a905a269c1d06cf
olaar09 <agboolar09@gmail.com>
2024-05-23 09:15:18 +0100

    ..

--- src/app/home/explore/components/Header.tsx
+++ src/app/home/explore/components/Header.tsx
@@ -14,7 +14,7 @@ const ExploreHeader = () => {
   };
 
   return (
-    <div className="flex h-12 items-center justify-between w-full px-2 lg:hidden">
+    <div className="flex h-12 items-center justify-between w-full px-2 ">
       <div className="flex items-center justify-start gap-x-6">
         <img className="w-6 h-6" src="/logo.png" alt="Logo" />
       </div>
