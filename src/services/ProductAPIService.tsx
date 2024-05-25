ac2d2086351fcc1796763ba4904da6ea91eb6bb5
olaar09 <agboolar09@gmail.com>
2024-05-25 22:47:22 +0100

    ..

--- src/services/ProductAPIService.tsx
+++ src/services/ProductAPIService.tsx
@@ -17,7 +17,7 @@ class ProductAPIService {
   }): Promise<string> {
     try {
       const response = await this.apiService.post<{ data: any }>(
-        `/products/${data.type === "data" ? "buy_data" : "buy_airtime"}`,
+        `/products/${data.type === "data" ? "buy_data" : "buy_airtime_data"}`,
         data
       );
       return response.data;
