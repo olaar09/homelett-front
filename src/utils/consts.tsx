5edd90289d98d07968c96f9d953447a184e3a8cd
olaar09 <agboolar09@gmail.com>
2024-06-12 12:00:09 +0100

    ..

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -2,7 +2,7 @@ export const Str = {
   earnChannel: "https://chat.whatsapp.com/J0YYtU74XkUE9eSKeaz2lV",
 
   whatsappHelp:
-    "https://api.whatsapp.com/send?phone=2347018135800&text=I%20need%20to%20help",
+    "https://api.whatsapp.com/send?phone=2347073106824&text=Hello%2C%20i%20need%20help",
   whatsapp:
     "https://api.whatsapp.com/send?phone=2347018135800&text=I%20want%20to%20start%20a%20subscription",
   CV_TEMPLATES: {
