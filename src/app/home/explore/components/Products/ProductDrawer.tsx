4e107d3383dc4481ed75396c4987545d22bf4842
olaar09 <agboolar09@gmail.com>
2024-06-18 21:48:44 +0100

    ..

--- src/app/home/explore/components/Products/ProductDrawer.tsx
+++ src/app/home/explore/components/Products/ProductDrawer.tsx
@@ -211,16 +211,16 @@ const ProductDrawer: React.FC<DrawerProps> = ({ product, onClose, open }) => {
           <div className="h-screen flex flex-col items-center justify-start gap-y-4 px-4 pt-10">
             <img src="/taken.svg" className=" w-40 h-40" />
             <p className="text-center">
-              Hi chief, all weekly slots for this plan are currently already
-              filled. but monthly slots are available.{" "}
+              Hi chief, all weekly slots for this plan are currently filled up,
+              but monthly slots are still available.{" "}
             </p>{" "}
             <Tag
               color="cyan"
               className="text-xs text-foreground-secondary text-center break-words text-wrap"
             >
-              If you're a first-time user, don't worry. 5k + users trust us for
-              their streaming subs and our monthly plan offers a much better
-              experience than weekly
+              If you're a first-time user, don't worry. 5k+ users trust us for
+              their streaming subs and our monthly plan offers a much better &
+              premium experience than weekly
             </Tag>
             <div className="flex items-center flex-col mt-6 justify-center gap-y-4">
               <Button
