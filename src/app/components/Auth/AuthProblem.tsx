314a15395f5d1d54ffe5d5756664a8fd50b638a8
olaar09 <agboolar09@gmail.com>
2024-06-30 13:19:09 +0100

    .

--- src/app/components/Auth/AuthProblem.tsx
+++ src/app/components/Auth/AuthProblem.tsx
@@ -1,12 +1,11 @@
+import { Str } from "@/utils/consts";
+
 const AuthProblem = () => {
   return (
     <div className="text-center  px-4 mx-auto w-full lg:w-5/12 mt-8">
       <span className=" text-foreground-secondary text-sm text-center">
         Problems with sign in?
-        <a
-          href="https://api.whatsapp.com/send?phone=2347018135800&text=I%20want%20to%20start%20a%20subscription"
-          className=" text-banner"
-        >
+        <a href={Str.whatsappHelp} className=" text-banner">
           {" "}
           Contact support{" "}
         </a>
