d78f6adbdcc69c070a0472c21f38d004af682511
olaar09 <agboolar09@gmail.com>
2024-08-18 12:34:36 +0100

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
+import { Str } from '@/utils/consts';
 import { Icon } from '@iconify/react/dist/iconify.js';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Home() {
@@ -24,9 +26,16 @@ export default function Home() {
           </button>
           {menuOpen && (
             <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg py-2">
-              <a href="#" className="block px-4 py-2 hover:bg-gray-200">Login</a>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-200">Create Account</a>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-200">Contact Support</a>
+              <Link href={'/login'}>
+                <span className="block px-4 py-2 hover:bg-gray-200">Login</span>
+              </Link>
+              <Link href={'/request-invite'}>
+                <span className="block px-4 py-2 hover:bg-gray-200">Create Account</span>
+              </Link>
+
+              <Link href={Str.whatsappHelp}>
+                <span className="block px-4 py-2 hover:bg-gray-200">Contact Support</span>
+              </Link>
             </div>
           )}
         </div>
@@ -39,12 +48,18 @@ export default function Home() {
           Korn money is a financial services platform, not a bank. Banking services are provided by Korn's bank partner(s). Prepaid debit cards issued by Safe Haven Microfinance Bank.
         </p>
       </div>
-
+      <div className='mt-10'>
+        <img className='w-40' src='/shopping-bag.png' />
+        <img className='w-40' src="/dollar.png" />
+      </div>
       <div className='flex flex-grow  flex-col justify-end pb-10'>
         <div className="mt-12 md:mt-16 ">
-          <button className="bg-white text-black py-2 px-4 rounded-full text-lg font-bold">
-            Click here to get started
-          </button>
+          <Link href={'/request-invite'}>
+            <button className="bg-white text-black py-2 px-4 rounded-full text-lg font-bold">
+              Click here to get started
+            </button>
+          </Link>
+
         </div>
       </div>
 
